0dae8248260b8b9b1b7b4f5dca9e2ec134d25335
Markus Leist <markus@lei.st>
2023-02-08 16:47:18 +0100

    tests for participant

---
+++ test/test_participant.py
@@ -0,0 +1,25 @@
+import unittest
+from uuid import UUID
+
+from model.participant import Participant
+
+
+class SimObjectTestCase(unittest.TestCase):
+    def setUp(self):
+        self.participant = Participant(el_role='no_role',
+                                       en_title='en_title',
+                                       de_title='de_title')
+
+    def test_01(self):
+        self.assertEqual(type(self.participant), Participant)
+        self.assertEqual(str(type(self.participant)),
+                         "<class 'model.participant.Participant'>")
+
+    def test_obj_has_valid_uuid(self):
+        uuid_obj = UUID(str(self.participant._uuid))
+        self.assertEqual(str(uuid_obj),
+                         str(self.participant._uuid))
+
+
+if __name__ == '__main__':
+    unittest.main()
