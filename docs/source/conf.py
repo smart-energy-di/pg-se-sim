411ff37590fd28011bebd7dfec1b8df86e891407
Markus Leist <markus@lei.st>
2023-02-21 14:56:00 +0100

    First attempts with multi-version docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,6 +2,8 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
+import sys
+import os
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -13,8 +15,6 @@ author = 'mleist'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-import os
-import sys
 
 sys.path.insert(0, os.path.abspath('../../src'))
 sys.path.insert(0, os.path.abspath('..'))
@@ -28,9 +28,9 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinxcontrib.plantuml',
     'sphinx.ext.autosummary',
+    'sphinx_multiversion',
     'sphinx_click',
 ]
-
 
 source_suffix = {
     '.rst': 'restructuredtext',
@@ -66,7 +66,6 @@ autosummary_generate = True  # Turn on sphinx.ext.autosummary
 
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
 myst_heading_anchors = 2
 myst_enable_extensions = [
     "colon_fence",
@@ -74,10 +73,33 @@ myst_enable_extensions = [
 ]
 suppress_warnings = ["myst.header"]
 
-
 # -- Options for todo extension ----------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/extensions/todo.html#configuration
 
 todo_include_todos = True
 
 today_fmt = '%Y-%m-%dT%H:%M:%S%z'
+
+# ----- Options sphinx-multiversion ------------------------------------------
+
+# Whitelist pattern for tags (set to None to ignore all tags)
+smv_tag_whitelist = r'^.*$'
+
+# Whitelist pattern for branches (set to None to ignore all branches)
+smv_branch_whitelist = r'^.*$'
+
+# Whitelist pattern for remotes (set to None to use local branches only)
+smv_remote_whitelist = None
+
+# Pattern for released versions
+# smv_released_pattern = r'^tags/.*$'           # Tags only
+# smv_released_pattern = r'^heads/\d+\.\d+$'    # Branches like "2.1"
+# smv_released_pattern = r'^(tags/.*|heads/\d+\.\d+)$'           # Branches like "2.1" and all tags
+# smv_released_pattern = r'^(heads|remotes/[^/]+)/(?!:main).*$'  # Everything except main branch
+
+
+# Format for versioned output directories inside the build directory
+smv_outputdir_format = '{ref.name}'
+
+# Determines whether remote or local git branches/tags are preferred if their output dirs conflict
+smv_prefer_remote_refs = False
